4fd8a3fe61ee0845c68a4e87d99d5575d5ad7f8b
tom <didymusthomas6@gmail.com>
2026-06-17 23:18:47 +0300

    Add flask api

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def root():
+    with open("version.txt", r) as file:
+        version = file.read().strip()
+
+    return render_template(
+        "index.html"
+        version=version,
+    )
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
